820bd4216a388c8fe7bcb8da4cfc2d1ee1e1b229
Hamza <muhammadhamzam1486@gmail.com>
2024-01-07 19:10:21 +0500

    Add solution function and is_evenly_divisible_by function

---
+++ day2/problem3.py
@@ -0,0 +1,18 @@
+"""
+PROBLEM:
+2520 is the smallest number than can be divided by the each of the numbers from 1 to 10 without any remainder.
+What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+"""
+
+
+def is_evenly_divisible_by(number: float, max_divider: float) -> bool:
+    pass
+
+
+def solution(max_divider: float) -> float:
+    pass
+
+
+if __name__ == "__main__":
+    print(solution(10))
+    print(solution(20))
