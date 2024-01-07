36296b03c43001137c0250e11c5e7358252f531d
Hamza <muhammadhamzam1486@gmail.com>
2024-01-07 17:36:11 +0500

    define problem and its test cases

---
+++ day2/problem2.py
@@ -0,0 +1,31 @@
+"""
+PROBLEM:
+The sum of squares of the first ten natural numbers is,
+1^2 + 2^2 + ... + 10^2 = 385
+
+The square of the sum of first 10 natural numbers is, 
+(1 + 2 + ... + 10) ^ 2 = 55^2 = 3025
+
+Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is,
+3025 - 385 = 2640
+
+Find the difference between the sum of the squares of the first one hundred natural numbers and the squares of the sum.
+
+"""
+
+
+def sum_of_squares_of(n: int) -> int:
+    pass
+
+
+def square_of_sum_of(n: int) -> int:
+    pass
+
+
+def solution(n: int) -> int:
+    return abs(square_of_sum_of(n) - sum_of_squares_of(n))
+
+
+if __name__ == "__main__":
+    print(solution(10))
+    print(solution(100))
---
+++ day2/problem2_test.py
@@ -0,0 +1,5 @@
+from problem2 import solution
+
+
+def test_solution():
+    assert solution(10) == 2640
