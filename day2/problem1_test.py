a79194f5bf82fec51215d3d57e2cacf2506d2b1a
Hamza <muhammadhamzam1486@gmail.com>
2024-01-07 15:50:12 +0500

    write tests

---
+++ day2/problem1_test.py
@@ -0,0 +1,16 @@
+from problem1 import fib, solution
+
+
+def test_fib():
+    assert fib(1) == 1
+    assert fib(2) == 1
+    assert fib(3) == 2
+    assert fib(4) == 3
+    assert fib(10) == 55
+
+
+def test_solution():
+    assert solution(10) == 10
+    assert solution(5) == 2
+    assert solution(89) == 44
+    assert solution(4_000_000) == 4613732
