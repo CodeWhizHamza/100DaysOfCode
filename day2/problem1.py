41d7e750d40b7177fd2e48a8c5e72358daaf906c
Hamza <muhammadhamzam1486@gmail.com>
2024-01-07 15:50:18 +0500

    implement basic solution

--- day2/problem1.py
+++ day2/problem1.py
@@ -5,3 +5,29 @@ Each new term in the Fibonacci sequence is generate by adding the previous two t
 
 By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even valued terms.
 """
+
+
+def fib(n: int) -> int:
+    if n <= 1:
+        return n
+
+    return fib(n - 1) + fib(n - 2)
+
+
+def solution(max_limit: int) -> int:
+    total = 0
+    i = 0
+    while True:
+        value = fib(i)
+        if value > max_limit:
+            break
+
+        if value % 2 == 0:
+            total += value
+        i += 1
+
+    return total
+
+
+if __name__ == "__main__":
+    print(solution(4000000))
