f9c03df5d5cb76c6611bb6a6bb312f7fb283dd10
Hamza <muhammadhamzam1486@gmail.com>
2024-01-08 21:04:24 +0500

    Add validation for non-positive numbers in solution() function And write the solution

--- day3/problem2.py
+++ day3/problem2.py
@@ -5,12 +5,29 @@ Find the largest palindrome made from the product of 3 digit numbers.
 """
 
 
+class CannotAcceptNonPositiveNumbers(Exception):
+    ...
+
+
 def is_palindrome(n: int) -> bool:
     return str(n)[::-1] == str(n)
 
 
 def solution(number_of_digits: int) -> int:
-    pass
+    if number_of_digits <= 0:
+        raise CannotAcceptNonPositiveNumbers()
+
+    max_limit = 10**number_of_digits - 1
+
+    largest_palindrome = 1
+
+    for i in range(1, max_limit + 1):
+        for j in range(1, max_limit + 1):
+            product = i * j
+            if is_palindrome(product) and product > largest_palindrome:
+                largest_palindrome = product
+
+    return largest_palindrome
 
 
 if __name__ == "__main__":
