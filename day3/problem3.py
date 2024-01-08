0a0b80cd34e8987f45a0e8e7fcbc1a3760bd93c8
Hamza <muhammadhamzam1486@gmail.com>
2024-01-08 21:07:35 +0500

    Add solution for finding the 10001st prime number

---
+++ day3/problem3.py
@@ -0,0 +1,18 @@
+"""
+PROBLEM:
+By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that 6th prime is 13.
+What is the 10001st prime number?
+"""
+
+
+def is_prime(n: int) -> bool:
+    pass
+
+
+def solution(n: int) -> int:
+    pass
+
+
+if __name__ == "__main__":
+    print(solution(6))
+    print(solution(10001))
