3848d6b483471a211ef9f169ced62f7681d23610
Hamza <muhammadhamzam1486@gmail.com>
2024-01-08 20:54:56 +0500

    Add tests for problem2 and solution

---
+++ day3/problem2_test.py
@@ -0,0 +1,15 @@
+from problem2 import is_palindrome, solution
+
+
+def test_is_palindrome():
+    assert is_palindrome(1) == True
+    assert is_palindrome(10) == False
+    assert is_palindrome(202) == True
+    assert is_palindrome(9009) == True
+    assert is_palindrome(205) == False
+    assert is_palindrome(123456654321) == True
+
+
+def test_solution():
+    assert solution(1) == 0
+    assert solution(2) == 9009
