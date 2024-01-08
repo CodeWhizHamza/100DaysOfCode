a097b46994f362a1a9db56fe2a6928042b4e064c
Hamza <muhammadhamzam1486@gmail.com>
2024-01-08 20:54:52 +0500

    Add solution for finding largest palindrome made from the product of 3 digit numbers

---
+++ day3/problem2.py
@@ -0,0 +1,18 @@
+"""
+PROBLEM:
+A palindromic number reads the same both ways. The largest palindrome made from the product of two 2 digit numbers is 9009 = 91 * 99.
+Find the largest palindrome made from the product of 3 digit numbers.
+"""
+
+
+def is_palindrome(n: int) -> bool:
+    pass
+
+
+def solution(number_of_digits: int) -> int:
+    pass
+
+
+if __name__ == "__main__":
+    print(solution(2))
+    print(solution(3))
