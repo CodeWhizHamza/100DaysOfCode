a71c930af2e4d0c37f08cd5cc4638b375153aa5f
Hamza <muhammadhamzam1486@gmail.com>
2024-01-08 20:46:22 +0500

    Add prime number validation and exception handling, and implement is prime function

--- day3/problem1.py
+++ day3/problem1.py
@@ -5,8 +5,25 @@ What is the largest prime factor of the number 600851475143
 """
 
 
+from math import sqrt
+
+
+class UnableToProcessNegativeNumbers(Exception):
+    ...
+
+
 def is_prime(n: int) -> bool:
-    pass
+    if n == 1:
+        return False
+
+    if n < 0:
+        raise UnableToProcessNegativeNumbers()
+
+    for i in range(2, int(sqrt(n))):
+        if n % i == 0:
+            return False
+
+    return True
 
 
 def solution(n: int) -> int:
