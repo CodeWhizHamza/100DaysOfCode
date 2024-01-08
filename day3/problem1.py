e22a7ea3e7a099241f6f719ebc2d04df0f2b2800
Hamza <muhammadhamzam1486@gmail.com>
2024-01-08 20:40:51 +0500

    Add problem1.py with initial code

---
+++ day3/problem1.py
@@ -0,0 +1,18 @@
+"""
+PROBLEM:
+The prime factors of 13195 are 5, 7, 13, and 29.
+What is the largest prime factor of the number 600851475143
+"""
+
+
+def is_prime(n: int) -> bool:
+    pass
+
+
+def solution(n: int) -> int:
+    pass
+
+
+if __name__ == "__main__":
+    print(solution(13195))
+    print(solution(600851475143))
