888f28f0bc48328f95b702c9b1c932b854ab9368
Hamza <muhammadhamzam1486@gmail.com>
2024-01-09 11:42:07 +0500

    Add tests for problem1 and solution

---
+++ day4/problem1_test.py
@@ -0,0 +1,10 @@
+from problem1 import solution, is_pythagorean_triplet
+
+
+def test_is_pythagorean_triplet():
+    assert is_pythagorean_triplet(3, 4, 5) is True
+    assert is_pythagorean_triplet(1, 2, 3) is False
+
+
+def test_solution():
+    assert solution() == 31875000
