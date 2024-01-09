40d2b51016e73e56936f695873e103413a01d70f
Hamza <muhammadhamzam1486@gmail.com>
2024-01-09 20:24:06 +0500

    day4: implement test cases

---
+++ day4/problem3_test.py
@@ -0,0 +1,15 @@
+from problem3 import solution, is_prime
+
+
+def test_is_prime():
+    assert is_prime(2) is True
+    assert is_prime(3) is True
+    assert is_prime(4) is False
+    assert is_prime(1) is False
+    assert is_prime(13) is True
+    assert is_prime(20) is False
+
+
+def test_solution():
+    assert solution(10) == 17
+    assert solution(2_000_000) == 0
