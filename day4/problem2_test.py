31805aa386c00cfa3327960a2d2336da06067463
Hamza <muhammadhamzam1486@gmail.com>
2024-01-09 12:05:55 +0500

    day4: define testcases for problem2

---
+++ day4/problem2_test.py
@@ -0,0 +1,11 @@
+from problem2 import solution, product
+
+
+def test_product():
+    assert product("9989") == 5832
+    assert product("1234") == 24
+
+
+def test_solution():
+    assert solution(4) == 5832
+    assert solution(13) == 0
