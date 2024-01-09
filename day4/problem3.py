2c8d69f2f39efb0a815d4cff7af3b5338352302a
Hamza <muhammadhamzam1486@gmail.com>
2024-01-09 20:50:44 +0500

    day4: solve problem 3

--- day4/problem3.py
+++ day4/problem3.py
@@ -4,12 +4,41 @@ Find the sum of all the primes below two million.
 """
 
 
+from math import sqrt
+
+
 def is_prime(n: int) -> bool:
-    pass
+    if n <= 1:
+        return False
+
+    if n <= 3:
+        return True
+
+    if n % 2 == 0 or n % 3 == 0:
+        return False
+
+    max_limit = int(sqrt(n)) + 1
+    for i in range(5, max_limit, 6):
+        if n % i == 0 or n % (i + 2) == 0:
+            return False
+
+    return True
 
 
 def solution(n: int) -> int:
-    pass
+    if n < 2:
+        return 0
+
+    if n == 2:
+        return 2
+
+    total = 2
+
+    for i in range(3, n + 1, 2):
+        if is_prime(i):
+            total += i
+
+    return total
 
 
 if __name__ == "__main__":
