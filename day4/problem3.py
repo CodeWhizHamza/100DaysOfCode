74e74ae1e160c26ab2939e057fc253f95d219840
Hamza <muhammadhamzam1486@gmail.com>
2024-01-09 20:18:22 +0500

    day4: define problem 3

---
+++ day4/problem3.py
@@ -0,0 +1,17 @@
+"""
+The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+Find the sum of all the primes below two million.
+"""
+
+
+def is_prime(n: int) -> bool:
+    pass
+
+
+def solution(n: int) -> int:
+    pass
+
+
+if __name__ == "__main__":
+    print(solution(10))
+    print(solution(2_000_000))
