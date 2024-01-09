f88b92a3ccf114d6d1f68d26c9e8450605c7d9d3
Hamza <muhammadhamzam1486@gmail.com>
2024-01-10 00:04:12 +0500

    day5: define solution structure

--- day5/problem1.py
+++ day5/problem1.py
@@ -3,3 +3,16 @@ PROBLEM: Power Digit Sum
 2^15 = 32768 and the sum of its digits is 26.
 What is the sum of the digits of 2^1000
 """
+
+
+def sum_of_digits(n: int) -> int:
+    pass
+
+
+def solution(power: int) -> int:
+    pass
+
+
+if __name__ == "__main__":
+    print(solution(15))
+    print(solution(1000))
