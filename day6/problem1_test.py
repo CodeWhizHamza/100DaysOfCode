afd315549003c284a2c44c17dfc5565f4e04aa55
Hamza <muhammadhamzam1486@gmail.com>
2024-01-12 23:26:57 +0500

    day6: write tests for problem 1

---
+++ day6/problem1_test.py
@@ -0,0 +1,16 @@
+from problem1 import factorial, solution, sum_of_digits
+
+
+def test_factorial():
+    assert factorial(1) == 1
+    assert factorial(5) == 120
+
+
+def sum_of_digits():
+    assert sum_of_digits(100) == 1
+    assert sum_of_digits(123) == 6
+
+
+def test_solution():
+    assert solution(10) == 27
+    assert solution(100) == 123
