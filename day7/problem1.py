9cbaf28e0ddcefeca98d754578ee90fd7c08e6d1
Hamza <muhammadhamzam1486@gmail.com>
2024-01-13 00:09:45 +0500

    day7: define structure for problem1

---
+++ day7/problem1.py
@@ -0,0 +1,16 @@
+"""
+PROBLEM: Highly Divisible Triangular Number
+"""
+
+
+def number_of_factors(number: int) -> int:
+    pass
+
+
+def solution(number_of_factors: int) -> int:
+    pass
+
+
+if __name__ == "__main__":
+    print(solution(5))
+    print(solution(500))
