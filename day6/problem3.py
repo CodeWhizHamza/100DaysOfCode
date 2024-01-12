84426f708aae5b0db643dddcf328da863f6ef10e
Hamza <muhammadhamzam1486@gmail.com>
2024-01-12 23:59:45 +0500

    day6: implement solution to problem 3

--- day6/problem3.py
+++ day6/problem3.py
@@ -4,11 +4,32 @@ PROBLEM: Longest Collatz Sequence
 
 
 def numbers_in_collatz_sequence_for(number: int) -> int:
-    pass
+    if number < 0:
+        return 0
+
+    count = 1
+
+    while number != 1:
+        if number % 2 == 0:
+            number //= 2
+        else:
+            number = 3 * number + 1
+        count += 1
+
+    return count
 
 
 def solution(max_limit: int) -> int:
-    pass
+    number_with_largest_chain = 0
+    largest_count = 0
+
+    for i in range(1, max_limit):
+        current_count = numbers_in_collatz_sequence_for(i)
+        if current_count > largest_count:
+            largest_count = current_count
+            number_with_largest_chain = i
+
+    return number_with_largest_chain
 
 
 if __name__ == "__main__":
