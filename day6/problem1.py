cacef3325c4ededd9e5f8f2c3ac7cb6beead9cac
Hamza <muhammadhamzam1486@gmail.com>
2024-01-12 23:25:07 +0500

    day6: define structure for problem 1

---
+++ day6/problem1.py
@@ -0,0 +1,20 @@
+"""
+PROBLEM: Factorial digit sum
+"""
+
+
+def factorial(number: int) -> int:
+    pass
+
+
+def sum_of_digits(number: int) -> int:
+    pass
+
+
+def solution(number: int) -> int:
+    pass
+
+
+if __name__ == "__main__":
+    print(solution(10))
+    print(solution(100))
