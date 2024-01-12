86875324bed53d345aceaaf3d9ae0ba65129834f
Hamza <muhammadhamzam1486@gmail.com>
2024-01-12 23:57:01 +0500

    day6: define structure for the solution

---
+++ day6/problem3.py
@@ -0,0 +1,16 @@
+"""
+PROBLEM: Longest Collatz Sequence
+"""
+
+
+def numbers_in_collatz_sequence_for(number: int) -> int:
+    pass
+
+
+def solution(max_limit: int) -> int:
+    pass
+
+
+if __name__ == "__main__":
+    print(solution(13))
+    print(solution(1_000_000))
