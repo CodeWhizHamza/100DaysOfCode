46fce08defdbab2e549f8fda61de68595b7b22ef
Hamza <muhammadhamzam1486@gmail.com>
2024-01-13 00:16:41 +0500

    day7: implement number of factors

--- day7/problem1.py
+++ day7/problem1.py
@@ -3,8 +3,20 @@ PROBLEM: Highly Divisible Triangular Number
 """
 
 
+from math import ceil, sqrt
+
+
 def number_of_factors(number: int) -> int:
-    pass
+    if number <= 1:
+        return 1
+
+    count = 2  # 1 and the number are included
+
+    for i in range(2, number // 2 + 1):
+        if number % i == 0:
+            count += 1
+
+    return count
 
 
 def solution(number_of_factors: int) -> int:
