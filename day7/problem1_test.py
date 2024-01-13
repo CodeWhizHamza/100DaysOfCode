27a658ec4beab9a574514a47a48ba89efb43d799
Hamza <muhammadhamzam1486@gmail.com>
2024-01-13 22:43:25 +0500

    day7: fix problem 1 test

--- day7/problem1_test.py
+++ day7/problem1_test.py
@@ -1,13 +1,6 @@
-from problem1 import number_of_factors, solution
-
-
-def test_number_of_factors():
-    assert number_of_factors(1) == 1
-    assert number_of_factors(2) == 2
-    assert number_of_factors(6) == 4
-    assert number_of_factors(28) == 6
+from problem1 import solution
 
 
 def test_solution():
     assert solution(5) == 28
-    assert solution(500) == 0
+    assert solution(500) == 76576500
