424b00a1e4c512a6e879edf73789c03a4894d348
Hamza <muhammadhamzam1486@gmail.com>
2024-01-14 19:21:34 +0500

    day8: implement iterative fibonacci function

--- day8/problem1.py
+++ day8/problem1.py
@@ -1,1 +1,16 @@
 # 1000-digit fibonacci number
+
+
+def fib(n: int) -> int:
+    if n <= 1:
+        return n
+
+    first = 0
+    second = 1
+
+    for i in range(n - 1):
+        temp = second
+        second = second + first
+        first = temp
+
+    return second
