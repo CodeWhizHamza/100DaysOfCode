801022c1e226ed4362497973f75ee831c9437b2f
Hamza <muhammadhamzam1486@gmail.com>
2024-01-15 19:32:37 +0500

    day9: define another function for sum of numbers

--- day9/problem1.py
+++ day9/problem1.py
@@ -3,10 +3,14 @@
 # what are the first 10 digits of the series ending at n=1000
 
 
-def solution(n: int) -> int:
+def sum_of_n_power_n(n: int) -> int:
     total = 0
 
     for i in range(1, n + 1):
         total += i**i
 
     return total
+
+
+def solution(n: int) -> int:
+    number = sum_of_n_power_n(n)
