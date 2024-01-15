8efa0ef6a0895a3dd2b287d51c5d4aba295e8486
Hamza <muhammadhamzam1486@gmail.com>
2024-01-15 19:31:44 +0500

    day9: write solution with full digits

--- day9/problem1.py
+++ day9/problem1.py
@@ -1,3 +1,12 @@
 # Self powers
 # The series 1^1 + 2^2 + 3^3 + ... + 10^10 = 10405071317
 # what are the first 10 digits of the series ending at n=1000
+
+
+def solution(n: int) -> int:
+    total = 0
+
+    for i in range(1, n + 1):
+        total += i**i
+
+    return total
