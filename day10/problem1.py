f2b1125889246a5beba659404469ac797cdc7071
Hamza <muhammadhamzam1486@gmail.com>
2024-01-16 19:50:47 +0500

    day10: implement is_abundant function and fix bug in get_factors

--- day10/problem1.py
+++ day10/problem1.py
@@ -6,13 +6,21 @@ from typing import List
 def get_factors(n: int) -> List[int]:
     factors = [1]
 
-    for number in range(2, n // 2):
+    for number in range(2, n // 2 + 1):
         if n % number == 0:
             factors.append(number)
 
     return factors
 
 
+def is_abundant(n: int) -> bool:
+    factors = get_factors(n)
+    sum_of_factors = sum(factors)
+    return sum_of_factors > n
+
+
 if __name__ == "__main__":
-    print(get_factors(1))
-    print(get_factors(28))
+    print(is_abundant(1))
+    print(is_abundant(16))
+    print(is_abundant(12))
+    print(is_abundant(24))
