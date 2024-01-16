ad75a9035904df648da6c6d1fcc33244887f057a
Hamza <muhammadhamzam1486@gmail.com>
2024-01-16 19:47:20 +0500

    day10: implement function for getting factors of a number in the form of a list

--- day10/problem1.py
+++ day10/problem1.py
@@ -1,1 +1,18 @@
 # Non abundant Sums
+
+from typing import List
+
+
+def get_factors(n: int) -> List[int]:
+    factors = [1]
+
+    for number in range(2, n // 2):
+        if n % number == 0:
+            factors.append(number)
+
+    return factors
+
+
+if __name__ == "__main__":
+    print(get_factors(1))
+    print(get_factors(28))
