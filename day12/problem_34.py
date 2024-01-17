9892ce0e33600530d82c2a31bfa5acf7cdd03992
Hamza <muhammadhamzam1486@gmail.com>
2024-01-17 11:34:51 +0500

    day12: implement solution to probem 34

--- day12/problem_34.py
+++ day12/problem_34.py
@@ -13,11 +13,25 @@ def factorial(n: int) -> int:
     return value
 
 
+FACTORIAL_OF_FIRST_10_NUMBERS = [factorial(i) for i in range(10)]
+
+
 def is_curious_number(n: int) -> bool:
     digits = [int(d) for d in str(n)]
-    sum_of_digits_factorial = sum([factorial(digit) for digit in digits])
+    sum_of_digits_factorial = sum(
+        [FACTORIAL_OF_FIRST_10_NUMBERS[digit] for digit in digits]
+    )
     return n == sum_of_digits_factorial
 
 
+def solution():
+    total = 0
+    for i in range(3, 10000000):
+        if is_curious_number(i):
+            total += i
+
+    return total
+
+
 if __name__ == "__main__":
-    pass
+    print(solution())
