91221714734f197102a795122117edf6b2cdbb47
Hamza <muhammadhamzam1486@gmail.com>
2024-01-18 11:06:04 +0500

    day11: implement solution and remove generic type from is_palindrome function

--- day11/problem_36.py
+++ day11/problem_36.py
@@ -1,11 +1,25 @@
 # problem 36
 # Double base palindromes
 
-from typing import TypeVar
 
-
-T = TypeVar("T")
-
-
-def is_palindrome(n: T) -> bool:
+def is_palindrome(n) -> bool:
     return str(n) == str(n)[::-1]
+
+
+def solution(high_limit: int) -> int:
+    """This function solves the  problem 36 of Project Euler
+
+    Args:
+        high_limit (int): upper limit exclusive
+
+    Returns:
+        int: sum of all the palindromic numbers in both bases
+    """
+    total: int = 0
+
+    for i in range(1, high_limit):
+        if is_palindrome(i) and is_palindrome(bin(i)[2:]):
+            print(i)
+            total += i
+
+    return total
