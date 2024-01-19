8b15c1953a72f1f554a490cd3a03b3e38a434746
Hamza <muhammadhamzam1486@gmail.com>
2024-01-19 11:43:57 +0500

    day13: implement solution

--- day13/problem_29.py
+++ day13/problem_29.py
@@ -5,3 +5,19 @@ Combination of all integers for a^b where 2 <= a, b <= 5 are:
 
 How many combination can be made for this exacty sequence, if lower_limit is 2 and upper_limit is 100.f
 """
+
+from typing import Set
+
+
+def solution() -> int:
+    numbers: Set[int] = set()
+
+    for i in range(2, 101):
+        for j in range(2, 101):
+            numbers.add(i**j)
+
+    return len(numbers)
+
+
+if __name__ == "__main__":
+    print(solution())
