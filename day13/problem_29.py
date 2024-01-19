7ecec797f0a2319eb882b54004f2a2f07b6b175a
Hamza <muhammadhamzam1486@gmail.com>
2024-01-19 11:41:19 +0500

    day13: define problem

---
+++ day13/problem_29.py
@@ -0,0 +1,7 @@
+# Problem 29
+"""
+Combination of all integers for a^b where 2 <= a, b <= 5 are:
+4, 8, 9, 16, 25, 27, 32, 64, 81, 125, 243, 256, 625, 1024, 31255.
+
+How many combination can be made for this exacty sequence, if lower_limit is 2 and upper_limit is 100.f
+"""
