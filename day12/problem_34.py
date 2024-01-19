60b9059e6fa3a76f9670c069968a072fc9278ebb
Hamza <muhammadhamzam1486@gmail.com>
2024-01-19 11:22:34 +0500

    day12: implement iterative factorial function

--- day12/problem_34.py
+++ day12/problem_34.py
@@ -1,3 +1,13 @@
 # Digit Factorials
 # 145 is a curious number as the sum of factorials of its digits is equal to 145.
 # find the sum of all numbers
+
+
+def factorial(n: int) -> int:
+    if n <= 1:
+        return 1
+
+    value = 1
+    for i in range(2, n + 1):
+        value *= i
+    return value
