616b85c852d6264605bf83eae72f5d86f0676bc8
Hamza <muhammadhamzam1486@gmail.com>
2024-01-19 11:24:59 +0500

    day12: implement is_curious number function

--- day12/problem_34.py
+++ day12/problem_34.py
@@ -11,3 +11,13 @@ def factorial(n: int) -> int:
     for i in range(2, n + 1):
         value *= i
     return value
+
+
+def is_curious_number(n: int) -> bool:
+    digits = [int(d) for d in str(n)]
+    sum_of_digits_factorial = sum([factorial(digit) for digit in digits])
+    return n == sum_of_digits_factorial
+
+
+if __name__ == "__main__":
+    pass
