7d510fa494e9f2827854d045cff0e994140ff93d
Hamza <muhammadhamzam1486@gmail.com>
2024-01-20 11:48:41 +0500

    implement sum of power function

--- day14/problem_30.py
+++ day14/problem_30.py
@@ -1,1 +1,7 @@
 # PROBLEM 30: Digit Fifth Powers
+
+
+def sum_of_digits_to_the_power(number: int, power: int) -> int:
+    digits = [int(digit) for digit in str(number)]
+    powers = [digit**power for digit in digits]
+    return sum(powers)
