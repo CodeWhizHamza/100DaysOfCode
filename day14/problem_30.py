81bd8efd2c11f2c7d4d36b74c385bd9f4d419e9a
Hamza <muhammadhamzam1486@gmail.com>
2024-01-20 11:49:32 +0500

    change return type

--- day14/problem_30.py
+++ day14/problem_30.py
@@ -1,7 +1,7 @@
 # PROBLEM 30: Digit Fifth Powers
 
 
-def statisfy_required_condition(number: int, power: int) -> int:
+def statisfy_required_condition(number: int, power: int) -> bool:
     digits = [int(digit) for digit in str(number)]
     powers = [digit**power for digit in digits]
     return sum(powers) == number
