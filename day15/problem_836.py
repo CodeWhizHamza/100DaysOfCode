4c8146c757cd78f4289996943cee7642fb49df8c
Hamza <muhammadhamzam1486@gmail.com>
2024-01-21 19:29:57 +0500

    define problem

---
+++ day15/problem_836.py
@@ -0,0 +1,22 @@
+# PROBLEM
+"""
+
+Let A be an <b>affine</b> <b>plane</b> over a <b>radically</b> <b>integral</b> <b>local</b> <b>field</b> F with residual characteristic p.
+
+We consider an <b>open</b> <b>oriented</b> <b>line</b> <b>section</b> U of A with normalized Haar measure m.
+
+Define f(m, p) as the maximal possible discriminant of the <b>jacobian</b> associated to the <b>orthogonal</b> <b>kernel</b> <b>embedding</b> of U into A.
+
+Find f(20230401, 57). Give as your answer the concatenation of the first letters of each bolded word.
+
+"""
+
+
+def f(m, p):
+    text = """Let A be an <b>affine</b> <b>plane</b> over a <b>radically</b> <b>integral</b> <b>local</b> <b>field</b> F with residual characteristic p.
+
+We consider an <b>open</b> <b>oriented</b> <b>line</b> <b>section</b> U of A with normalized Haar measure m.
+
+Define f(m, p) as the maximal possible discriminant of the <b>jacobian</b> associated to the <b>orthogonal</b> <b>kernel</b> <b>embedding</b> of U into A.
+
+Find f(20230401, 57). Give as your answer the concatenation of the first letters of each bolded word."""
