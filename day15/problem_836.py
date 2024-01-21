d9f41555c8e735b0c6a87992ae895ffa652a6818
Hamza <muhammadhamzam1486@gmail.com>
2024-01-21 19:33:10 +0500

    implement solution

--- day15/problem_836.py
+++ day15/problem_836.py
@@ -20,7 +20,13 @@ We consider an <b>open</b> <b>oriented</b> <b>line</b> <b>section</b> U of A wit
 Define f(m, p) as the maximal possible discriminant of the <b>jacobian</b> associated to the <b>orthogonal</b> <b>kernel</b> <b>embedding</b> of U into A.
 
 Find f(20230401, 57). Give as your answer the concatenation of the first letters of each bolded word."""
+    tokens = text.split("<b>")
+    tokens_with_ending_bold_tag = list(filter(lambda token: "</b>" in token, tokens))
+    first_characters = list(map(lambda token: token[0], tokens_with_ending_bold_tag))
+    answer = "".join(first_characters)
+    return answer
 
 
 if __name__ == "__main__":
-    f(20230401, 57)
+    answer = f(20230401, 57)
+    print(answer)
