9455fd920c4b79a2eea59d9a7ac3e0c6e23ab8c4
Hamza <muhammadhamzam1486@gmail.com>
2024-01-25 20:52:46 +0500

    implement the basic permutations generator

--- day16/problem_24.py
+++ day16/problem_24.py
@@ -1,1 +1,19 @@
 # PROBLEM: Lexicographic Permutations
+
+
+def get_permutations():
+    permutations = []
+    for i in range(3):
+        for j in range(3):
+            if j == i:
+                continue
+            for k in range(3):
+                if k == j or k == i:
+                    continue
+                permutations.append(f"{i}{j}{k}")
+
+    print(permutations)
+
+
+if __name__ == "__main__":
+    get_permutations()
